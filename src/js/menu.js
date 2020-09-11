889f92d62e3b3000c3f4bab137ef14ccbfa18881
Julia Pospelova <michiaro@bk.ru>
2020-09-11 18:53:56 +0500

    fix menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -9,16 +9,22 @@ const hamburgerMenu = document.querySelector('[data-hamburger-menu]');
 
 const activeClass = 'is-active';
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle(activeClass);
-  hamburgerMenu.classList.toggle(activeClass);
-});
-catalogToggler.addEventListener('click', () => {
-  catalogMenu.classList.toggle(activeClass);
-});
-catalogMobileToggler.addEventListener('click', () => {
-  catalogMenu.classList.toggle(activeClass);
-});
+if (hamburger) {
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle(activeClass);
+    hamburgerMenu.classList.toggle(activeClass);
+  });
+}
+if (catalogToggler) {
+  catalogToggler.addEventListener('click', () => {
+    catalogMenu.classList.toggle(activeClass);
+  });
+}
+if (catalogMobileToggler) {
+  catalogMobileToggler.addEventListener('click', () => {
+    catalogMenu.classList.toggle(activeClass);
+  });
+}
 
 // задаем ширину бургер-меню на десктопе
 function setBurgerMenuWidth() {
