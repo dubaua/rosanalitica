1dee8160a84d83e6fb19cafd20f066b77339927a
Julia Pospelova <michiaro@bk.ru>
2020-09-15 01:17:23 +0500

    add page about

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -10,7 +10,7 @@ const isDev = process.env.NODE_ENV === 'development';
  * 1. создаем новый файл в @/pages
  * 2. добавляем его имя сторокой в массив pageNameArray
  */
-const pageNameArray = ['index'];
+const pageNameArray = ['index', 'about'];
 
 const pageHtmlWebpackPluginConfigArray = pageNameArray.map((entryName) => {
   return new HtmlWebpackPlugin({
