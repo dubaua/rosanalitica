54227ddad48852be8f2ba2f1601f9102918a6dae
Julia Pospelova <michiaro@bk.ru>
2020-09-15 02:01:44 +0500

    init main slider only if slides more than 1

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,21 +2,39 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 
 Swiper.use([Navigation, Pagination]);
 
-//TODO если элемент один, то слайдер не нужен
+const swiperContainer = document.querySelector('.main-swiper-container');
 
-const mainSlider = new Swiper('.main-swiper-container', {
-  loop: true,
-  roundLengths: true,
+function initMainSlider() {
+  new Swiper('.main-swiper-container', {
+    loop: true,
+    roundLengths: true,
 
-  pagination: {
-    el: '.slider__pagination',
-    clickable: true,
-  },
+    pagination: {
+      el: '.slider__pagination',
+      clickable: true,
+    },
 
-  navigation: {
-    prevEl: '.slider__navigation-button--prev',
-    nextEl: '.slider__navigation-button--next',
-  },
-});
+    navigation: {
+      prevEl: '.slider__navigation-button--prev',
+      nextEl: '.slider__navigation-button--next',
+    },
+  });
+}
 
-console.log('slider');
+if (swiperContainer) {
+  const slideNodeList = swiperContainer.querySelectorAll('.swiper-slide');
+  const slideArray = Array.from(slideNodeList);
+  const sliderNavigation = document.querySelector(
+    '[data-main-slider-navigation]',
+  );
+  const sliderPagination = document.querySelector(
+    '[data-main-slider-pagination]',
+  );
+
+  if (slideArray.length > 1) {
+    initMainSlider();
+  } else {
+    sliderNavigation.style.display = 'none';
+    sliderPagination.style.display = 'none';
+  }
+}
