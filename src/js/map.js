76eefd904e68a6e4bdc7c092d8ef4d6cef8f9f60
Vladimir Lysov <dubaua@gmail.com>
2020-11-28 00:09:25 +0500

    add configurable map

--- src/js/map.js
+++ src/js/map.js
@@ -1,15 +1,20 @@
 import L from 'leaflet/dist/leaflet.js';
 
-const mapNodeId ='map';
+const mapNodeId = 'map';
 
-if (document.getElementById(mapNodeId)) {
-  const accessToken =
-    'pk.eyJ1IjoibWljaGlhcm8iLCJhIjoiY2tlYWJpb3JvMGI0ODJ5dDB6dTR3enhuayJ9.lc4KAfa-6vpkA7FGJD1-pA';
+const mapNode = document.getElementById(mapNodeId);
+
+if (mapNode) {
+  const lat = mapNode.getAttribute('data-lat');
+  const lon = mapNode.getAttribute('data-lon');
+  const zoom = mapNode.getAttribute('data-zoom');
+
+  const accessToken = 'pk.eyJ1IjoibWljaGlhcm8iLCJhIjoiY2tlYWJpb3JvMGI0ODJ5dDB6dTR3enhuayJ9.lc4KAfa-6vpkA7FGJD1-pA';
   const attribution = `© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>`;
   const styleUrl = `https://api.mapbox.com/styles/v1/michiaro/ckf8rkp9n5s1s19qnje23jtn4/tiles/{z}/{x}/{y}?access_token=${accessToken}`;
   const pinUrl = 'http://www.rosanalitica.dubaua.ru/images/map-pin.png';
 
-  const mapInstance = L.map('map').setView([59.883644, 30.330233], 16);
+  const mapInstance = L.map('map').setView([lat, lon], zoom);
   const tileLayer = L.tileLayer(styleUrl, { attribution });
   tileLayer.addTo(mapInstance);
 
@@ -18,7 +23,8 @@ if (document.getElementById(mapNodeId)) {
   const customMarker = L.icon({
     iconUrl: pinUrl,
     iconSize: [44, 62],
+    iconAnchor: [22, 62],
   });
-  const marker = L.marker([59.883644, 30.330233], { icon: customMarker });
+  const marker = L.marker([lat, lon], { icon: customMarker });
   marker.addTo(mapInstance);
 }
